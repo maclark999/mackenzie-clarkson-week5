ccfb1cca9f5a4451a669fa73099189c6d11fba84
mackenzie clarkson <mac3@mackenzies-MacBook-Pro.local>
2015-02-09 15:50:40 -0700

    add events controller and model connections

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,51 @@
 class EventsController < ApplicationController
+before_action :set_event, only: [:show, :edit, :update, :destroy]
+before_action :set_location
+
   def new
+    @event = Event.new
+    @location = Location.find(params[:location_id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.location_id = params[:location_id]
+    if @event.save
+      redirect_to location_path(@location), notice: "Location was successfully created!"
+    end
   end
 
   def show
   end
 
   def edit
+
   end
-  
+
+  def update
+    if @event.save
+      redirect_to @location
+    end
+  end
+
+  def destroy
+    if @event.destroy
+      redirect_to location_path
+    end
+  end
+
+private
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def set_location
+    @location = Location.find(params[:location_id])
+  end
+
+  def event_params
+    params.require(:event).permit(:description, :date, :requires_id, :location_id)
+  end
+
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,12 @@
 class LocationsController < ApplicationController
+before_action :set_location, only: [:show, :edit, :update, :destroy]
   def index
     @locations = Location.all
   end
 
   def show
     @location = Location.find(params[:id])
+    @events = Location.find(params[:id]).events
   end
 
   def new
@@ -14,18 +16,36 @@ class LocationsController < ApplicationController
   def create
     @location = Location.new(location_params)
     if @location.save
-      redirect_to location_path
+      redirect_to location_path(@location)
     else
       render ('new')
     end
   end
 
   def edit
+
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      redirect_to location_path(@location)
+    end
+  end
+
+  def destroy
+    if @location.destroy
+      redirect_to locations_path, notice: "Are you sure?"
+    end
   end
 
 private
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
     def location_params
-      params.require(:location).permit(:name, :address, :zip_code)
+      params.require(:location).permit(:name, :address, :zipcode)
     end
 
 end
